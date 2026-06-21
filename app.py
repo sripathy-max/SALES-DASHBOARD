2027b43000b4e0a59234b6e724ad47749d79ff0e
sripathy-max <sripathygrouppharma@gmail.com>
2026-06-21 13:32:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
+import os
+import json
+from google.oauth2 import service_account
 
+# Read the JSON from the environment variable we set in Render
+creds_dict = json.loads(os.environ['GOOGLE_CREDENTIALS_JSON'])
+creds = service_account.Credentials.from_service_account_info(creds_dict)
+
+# Now use this 'creds' object to connect to gspread/sheets
+client = gspread.authorize(creds)
